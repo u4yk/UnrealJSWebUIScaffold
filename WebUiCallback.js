ede80d3b3bf2a9d546c83d44b82f7b9ad766b02e
Chuck Ringoringo <2370767+u4yk@users.noreply.github.com>
2022-06-20 13:59:43 -0400

    removed console.logs

--- WebUiCallback.js
+++ WebUiCallback.js
@@ -11,10 +11,9 @@ const capitalize = s => s.charAt(0).toUpperCase() + s.slice(1)
 const callbackConstructor = function () {
     stateList.forEach(v => {
         const module = require(`./states/${v}`)
-        // console.log(this)
         for (const [k, p] of Object.entries(module)) {
             const fx = `update${capitalize(v)}${capitalize(k)}`
-            // console.log(`Callback Constructor:: method added -- ${fx}`)
+
             if (!this[fx]) {
                 this[fx] = function (data) {}.bind(this)
             }
